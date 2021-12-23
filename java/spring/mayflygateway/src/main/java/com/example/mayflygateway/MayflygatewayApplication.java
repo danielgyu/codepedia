ff2623d676ba902bd71631eb4ad0431fea7f3b87
kungyu <hismayfly@gmail.com>
2021-12-23 23:50:53 +0900

    testing out zuul

---
+++ java/spring/mayflygateway/src/main/java/com/example/mayflygateway/MayflygatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.mayflygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayflygatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayflygatewayApplication.class, args);
+	}
+
+}
---
+++ java/spring/mayflygateway/src/test/java/com/example/mayflygateway/MayflygatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mayflygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayflygatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
