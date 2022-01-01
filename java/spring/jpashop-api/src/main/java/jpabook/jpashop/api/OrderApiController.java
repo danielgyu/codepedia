2e528c150490fad49d8d979d75d13c7cfaeea020
kungyu <hismayfly@gmail.com>
2022-01-01 23:48:46 +0900

    lazy loading finished

--- java/spring/jpashop-api/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ java/spring/jpashop-api/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,12 +2,10 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
-import jpabook.jpashop.repository.OrderQueryDTO;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
-import jpabook.jpashop.service.OrderService;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,69 +15,64 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/*
- * ManyToOne, One-to-One; X-to-One
- * Order
- * Order -> Member
- * Order -> Delivery
- */
 @RestController
 @RequiredArgsConstructor
-public class OrderSimpleController {
+public class OrderApiController {
 
     private final OrderRepository orderRepository;
-    // private final OrderService orderService;
 
-    @GetMapping("/api/v1/simple-orders")
     public List<Order> getOrdersV1() {
-        List<Order> allOrders = orderRepository.findAllByString(new OrderSearch());
-        // List<Order> allOrders = orderService.findOrders(new OrderSearch());
-        for (Order order : allOrders) {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : orders) {
             order.getMember().getName();
             order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
         }
-        return allOrders;
+        return orders;
     }
 
-    @GetMapping("/api/v2/simple-orders")
-    public List<OrderDTO> ordersV2() {
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> getOrdersV2() {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
-        List<OrderDTO> result = orders.stream()
-                .map(o -> new OrderDTO(o))
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
 
         return result;
-    }
-
-    @GetMapping("/api/v3/simple-orders")
-    public List<OrderDTO> ordersV3() {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery();
-        List<OrderDTO> result = orders.stream()
-                .map(o -> new OrderDTO(o))
-                .collect(Collectors.toList());
-
-        return result;
-    }
-
-    @GetMapping("/api/v4/simple-orders")
-    public List<OrderQueryDTO> ordersV4() {
-        return orderRepository.findOrderDTO();
     }
 
     @Data
-    static class OrderDTO {
+    static class OrderDto {
         private Long orderId;
         private String name;
         private LocalDateTime orderDate;
         private OrderStatus orderStatus;
         private Address address;
+        private List<OrderItemDto> orderItems;
 
-        public OrderDTO(Order o) {
+        public OrderDto(Order o) {
             orderId = o.getId();
             name = o.getMember().getName();
             orderDate = o.getOrderDate();
             orderStatus = o.getStatus();
             address = o.getDelivery().getAddress();
+            orderItems = o.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
         }
     }
 }
