66a0bdafbe1ca4c80eb1b34603cd99ba86a7c53d
kungyu <hismayfly@gmail.com>
2022-01-07 23:51:47 +0900

    non constructible change

--- java/coding-interview/src/com/mayfly/interview/arrays/NonConstructibleChange.java
+++ java/coding-interview/src/com/mayfly/interview/arrays/NonConstructibleChange.java
@@ -1,5 +1,6 @@
 package com.mayfly.interview.arrays;
 
+import java.util.Arrays;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -12,30 +13,15 @@ public class NonConstructibleChange {
     }
 
     public int nonConstructibleChange(int[] coins) {
-        if (coins.length == 0) {
-            return 1;
-        }
+        Arrays.sort(coins);
 
-        SortedSet<Integer> changeSet = new TreeSet<>();
-
-        for (int i = 0; i < coins.length - 1; i++) {
-            int currentChange = coins[i];
-            changeSet.add(currentChange);
-
-            for (int j = i + 1; j < coins.length; j++) {
-                currentChange += coins[j];
-                changeSet.add(currentChange);
+        int change = 0;
+        for (int coin : coins) {
+            if (coin > change + 1) {
+                return change + 1;
             }
+            change += coin;
         }
-
-        System.out.println("changeSet = " + changeSet);
-
-        for (int s = 1; s < changeSet.size(); s++) {
-            if (!changeSet.contains(s)) {
-                return s;
-            }
-        }
-
-        return -1;
+        return change + 1;
     }
 }
