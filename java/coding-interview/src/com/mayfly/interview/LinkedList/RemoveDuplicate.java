3a523bbf66f5332623ec2d25f029a1dd84f18ca6
kungyu <hismayfly@gmail.com>
2022-01-15 12:03:54 +0900

    remove duplicate in linked list

---
+++ java/coding-interview/src/com/mayfly/interview/LinkedList/RemoveDuplicate.java
@@ -0,0 +1,28 @@
+package com.mayfly.interview.LinkedList;
+
+public class RemoveDuplicate {
+    public static class LinkedList {
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+        while (linkedList != null) {
+            LinkedList next = linkedList.next;
+
+            while (next != null && linkedList.value == next.value) {
+                next = next.next;
+            }
+
+            linkedList.next = next;
+            linkedList = linkedList.next;
+        }
+
+        return null;
+    }
+}
