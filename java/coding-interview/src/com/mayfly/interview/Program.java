5a44757de7894d95cb9f63eef5a3a6a9b190c60b
kungyu <hismayfly@gmail.com>
2022-01-17 23:08:21 +0900

    product sum

--- java/coding-interview/src/com/mayfly/interview/Program.java
+++ java/coding-interview/src/com/mayfly/interview/Program.java
@@ -1,10 +1,10 @@
 package com.mayfly.interview;
 
 import com.mayfly.interview.Recursion.Fibonacci;
+import com.mayfly.interview.Recursion.ProductSum;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println(Fibonacci.getNthFib(500));
-        //System.out.println(Fibonacci.getFibWithoutMemo(500));
+        ProductSum.run();
     }
 }
---
+++ java/coding-interview/src/com/mayfly/interview/Recursion/ProductSum.java
@@ -0,0 +1,50 @@
+package com.mayfly.interview.Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductSum {
+    public static void run() {
+        /*
+         * [5, 2, [7, -1], 3, [6, [-13, 8], 4]]
+         * 5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4)
+         * 7 + 12 + 3 - 10 = 12
+         * 5 + 2 + 12 + 3 - 10 = 12
+         */
+        List<Object> array = Arrays.asList(
+                5,
+                2,
+                Arrays.asList(7, -1),
+                3,
+                Arrays.asList(6, Arrays.asList(-13, 8), 4)
+        );
+
+        System.out.println("array = " + array);
+        System.out.println("productSum = " + productSum(array));
+    }
+
+    public static int productSum(List<Object> array) {
+        int totalSum = 0;
+        int depth = 1;
+        for (Object o : array) {
+            totalSum += getSum(o, depth);
+        }
+
+        return totalSum;
+    }
+
+    public static int getSum(Object object, int depth) {
+        if (object instanceof Integer) {
+            return (int) object;
+        }
+
+        int sum = 0;
+        depth += 1;
+        for (Object o : (List) object) {
+            sum += getSum(o, depth);
+        }
+
+        return sum * depth;
+    }
+}
