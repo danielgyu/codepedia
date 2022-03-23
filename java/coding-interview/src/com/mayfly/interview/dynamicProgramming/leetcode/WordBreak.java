3dc26e8a7c04cde48f48a5dfee7f42cf39058d2b
kungyu <hismayfly@gmail.com>
2022-03-24 02:10:32 +0900

    word break

--- java/coding-interview/src/com/mayfly/interview/dynamicProgramming/leetcode/WordBreak.java
+++ java/coding-interview/src/com/mayfly/interview/dynamicProgramming/leetcode/WordBreak.java
@@ -11,8 +11,31 @@ public class WordBreak {
                 return s2.length() - s1.length();
             }
         });
-        //return bfs(s, wordDict);
-        return dfs(s, wordDict);
+        return bfsOptimized(s, wordDict);
+        //return bfs(s, wordDict, 0);
+        //return dfs(s, wordDict);
+    }
+
+    public static boolean bfsOptimized(String s, List<String> wordDict) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+
+        Set<Integer> visited = new HashSet<>();
+        visited.add(0);
+
+        while (!queue.isEmpty()) {
+            Integer cur = queue.poll();
+
+            for (int i = cur + 1; i <= s.length(); i++) {
+                if (visited.contains(i)) continue;
+                if (wordDict.contains(s.substring(cur, i))) {
+                    if (i == s.length()) return true;
+                    visited.add(i);
+                    queue.add(i);
+                }
+            }
+        }
+        return false;
     }
 
     public static boolean dfs(String s, List<String> wordDict) {
@@ -45,20 +68,30 @@ public class WordBreak {
         return false;
     }
 
-    public static boolean bfs(String s, List<String> wordDict) {
-        System.out.println("wordDict = " + wordDict);
-        Queue<String> queue = new LinkedList<>();
-        queue.add(s);
+    public static boolean bfs(String s, List<String> wordDict, int idx) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(idx);
+
+        Set<Integer> visited = new HashSet<>();
+        visited.add(idx);
 
         while (!queue.isEmpty()) {
-            String cur = queue.poll();
+            Integer curIdx = queue.poll();
+            if (visited.contains(curIdx)) continue;
+
+            int before = queue.size();
 
             for (String word : wordDict) {
-                if (cur.equals(word)) return true;
-                else if (cur.startsWith(word)) {
-                    String leftover = cur.substring(word.length());
-                    queue.add(leftover);
+                Integer sumIdx = curIdx + word.length();
+
+                if (sumIdx <= s.length() && s.substring(curIdx, sumIdx).equals(word)) {
+                    if (sumIdx == s.length()) return true;
+                    queue.add(sumIdx);
                 }
+            }
+            if (queue.size() == before) {
+                System.out.println("adding to visited=" + curIdx);
+                visited.add(curIdx);
             }
         }
 
@@ -66,9 +99,10 @@ public class WordBreak {
     }
 
     public static void run() {
-        //System.out.println(solution("applepenapple", new ArrayList<>(Arrays.asList("apple", "pen")))); // true
-        //System.out.println(solution("catsandog", new ArrayList<>(Arrays.asList("cats", "dog", "sand", "and", "cat")))); // false
-        //System.out.println(solution("ccbb", new ArrayList<>(Arrays.asList("bc", "cb")))); // false
+        System.out.println(solution("applepenapple", new ArrayList<>(Arrays.asList("apple", "pen")))); // true
+        System.out.println(solution("catsandog", new ArrayList<>(Arrays.asList("cats", "dog", "sand", "and", "cat")))); // false
+        System.out.println(solution("ccbb", new ArrayList<>(Arrays.asList("bc", "cb")))); // false
         System.out.println(solution("catskicatcats", new ArrayList<>(Arrays.asList("cats", "cat", "dog", "ski")))); // true
+        System.out.println(solution("a", new ArrayList<>(Arrays.asList("b")))); // false
     }
 }
