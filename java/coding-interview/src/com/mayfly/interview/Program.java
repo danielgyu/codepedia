b9492ca06fd1dbfc7a3087bf5d1567c7a4a8b5da
kungyu <hismayfly@gmail.com>
2022-03-26 08:59:41 +0900

    [programmers/java] immigration

--- java/coding-interview/src/com/mayfly/interview/Program.java
+++ java/coding-interview/src/com/mayfly/interview/Program.java
@@ -4,12 +4,13 @@ import com.mayfly.interview.dynamicProgramming.MinNumberOfCoinsForChange;
 import com.mayfly.interview.dynamicProgramming.leetcode.LongestCommonSubsequence;
 import com.mayfly.interview.dynamicProgramming.leetcode.WordBreak;
 import com.mayfly.interview.graphs.leetcode.CourseSchedule;
+import com.mayfly.interview.programmers.levelthree.Immigration;
 
 public class Program {
     public static void main(String[] args) {
         //LongestIncreasingSubsequence.run();
         //LongestCommonSubsequence.run();
         //WordBreak.run();
-        CourseSchedule.run();
+        Immigration.run();
     }
 }
---
+++ java/coding-interview/src/com/mayfly/interview/programmers/levelthree/Immigration.java
@@ -0,0 +1,46 @@
+package com.mayfly.interview.programmers.levelthree;
+
+import java.util.Arrays;
+
+public class Immigration {
+    public static long solution(int n, int[] times) {
+        Arrays.sort(times);
+
+        long min = 1;
+        long max = (long)times[times.length-1] * n;
+        long mid;
+        long sum;
+        long answer = max;
+
+        System.out.println("min=" + min + " " + "max=" + max);
+        while (min <= max) {
+            sum = 0;
+            mid = (min + max) / 2;
+            System.out.println("sum=" + sum + " " + "mid=" + mid);
+
+            for (int time: times) {
+                sum += mid / time;
+                System.out.println("sum = " + sum);
+            }
+
+            if (sum >= n) {
+                if (mid < answer) {
+                    answer = mid;
+                }
+                max = mid -1;
+            } else {
+                min = mid + 1;
+            }
+
+            System.out.println();
+        }
+        return answer;
+    }
+
+    public static void run() {
+        //System.out.println(solution(6, new int[]{7, 10}));
+        //System.out.println(solution(1, new int[]{8, 90, 78}));
+        //System.out.println(solution(2, new int[]{8, 90, 78}));
+        System.out.println(solution(3, new int[]{2, 3}));
+    }
+}
