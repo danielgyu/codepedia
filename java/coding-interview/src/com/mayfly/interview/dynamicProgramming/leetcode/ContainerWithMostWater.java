6eb852367bee1a1105f17345f3150d257a6bbadd
kungyu <hismayfly@gmail.com>
2022-04-13 00:03:19 +0900

    [programmers/python] task assignment

---
+++ java/coding-interview/src/com/mayfly/interview/dynamicProgramming/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,19 @@
+package com.mayfly.interview.dynamicProgramming.leetcode;
+
+public class ContainerWithMostWater {
+    public static int solution(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int max = 0;
+
+        while (left < right) {
+
+        }
+
+        return max;
+    }
+
+    public static void run() {
+        System.out.println(solution(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+}
