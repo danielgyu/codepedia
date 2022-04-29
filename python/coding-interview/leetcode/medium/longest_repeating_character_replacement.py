031a48f313357f2317236a72106b5012d995a667
kungyu <hismayfly@gmail.com>
2022-04-30 00:14:04 +0900

    [leetcode/python] longest repeating character replacement finish

--- python/coding-interview/leetcode/medium/longest_repeating_character_replacement.py
+++ python/coding-interview/leetcode/medium/longest_repeating_character_replacement.py
@@ -5,11 +5,19 @@ def solution(s, k):
     count = defaultdict(int)
     res = 0
 
-    left, maxf = 0, 0
+    l, maxf = 0, 0
     for r in range(len(s)):
         cur = s[r]
         count[cur] += 1
         maxf = max(maxf, count[cur])
+
+        while (r - l + 1) - maxf > k:
+            count[s[l]] -= 1
+            l += 1
+
+        res = max(res, r - l + 1)
+    print(f"{res=}, {count=}")
+    return res
 
 assert solution("ABAB", 2) == 4
 assert solution("AABABBA", 1) == 4
