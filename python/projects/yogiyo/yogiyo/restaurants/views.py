4fb990361e90e73078a8c4175d31820434ca4bcc
kungyu <hismayfly@gmail.com>
2022-05-30 23:58:09 +0900

    [python/projects] all objects

--- python/projects/yogiyo/yogiyo/restaurants/views.py
+++ python/projects/yogiyo/yogiyo/restaurants/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from restaurants.models import Restaurants, Dishes
 
@@ -7,4 +7,6 @@ def healthcheck(request):
     return HttpResponse("healthy")
 
 
-def
+def all_restaurants():
+    restaurants = Restaurants.objects.all()
+    return {"name": r.name for r in restaurants}
