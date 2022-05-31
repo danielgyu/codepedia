7c9a71402297a4e30c30eda2acd536e6fc288ad7
kungyu <hismayfly@gmail.com>
2022-05-31 23:52:25 +0900

    [python/leetcode] longest palindrome substring

---
+++ python/coding-interview/leetcode/medium/longest_palindrome_substring_5.py
@@ -0,0 +1,31 @@
+def solution(s):
+    if len(s) == 1:
+        return s
+
+    if len(s) == 2:
+        return s[0] if s[0] != s[1] else s
+
+    def check_palindrome(left, right):
+        palindrome = s[left]
+        while left >= 0 and right <= len(s)-1:
+            if s[left] == s[right]:
+                palindrome = s[left:right+1]
+                left -= 1
+                right +=1
+            else:
+                break
+        return palindrome
+
+    longest = s[0]
+    for i in range(1, len(s)):
+        even = check_palindrome(i, i+1)
+        odd = check_palindrome(i-1, i+1)
+        longest = sorted([longest, even, odd], key=len, reverse=True)[0]
+
+    print(f"longest: {longest}")
+    return longest
+
+
+if __name__ == "__main__":
+    solution("babad")
+    solution("cbbd")
