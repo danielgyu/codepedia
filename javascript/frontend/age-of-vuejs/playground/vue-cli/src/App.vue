2f051d83d90b3d671e57e5ef89491aedccd49928
kungyu <hismayfly@gmail.com>
2022-11-05 10:04:04 +0900

    [JS] age of vuejs

---
+++ javascript/frontend/age-of-vuejs/playground/vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <app-header v-bind:propsdata="str"></app-header>
+    <app-footer v-on:footerEvent="logText"></app-footer>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  data: function() {
+    return {
+      str: 'Header!!!',
+    }
+  },
+  components: {
+    'app-header': AppHeader,
+    'app-footer': AppFooter,
+  },
+  methods: {
+    logText: function() {
+      console.log("received event");
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ javascript/frontend/age-of-vuejs/playground/vue-cli/src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <button v-on:click="passEvent">send</button>
+</template>
+
+<script>
+export default {
+    methods: {
+        passEvent: function() {
+            this.$emit('footerEvent')
+        }
+    }
+}
+</script>
+
+<style>
+</style>
