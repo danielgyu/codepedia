785f2ae2045e5ccd4d72363fd475eeb9be2a2aa8
kungyu.lee <kungyu.lee@wesang.com>
2023-05-06 10:33:00 +0900

    [java][algo] program.java

--- java/coding-interview/src/com/mayfly/interview/Program.java
+++ java/coding-interview/src/com/mayfly/interview/Program.java
@@ -1,9 +1,10 @@
 package com.mayfly.interview;
 
 import com.mayfly.interview.searching.FaultyUsers;
+import com.mayfly.interview.recursion.VowelDictionary;
 
 public class Program {
     public static void main(String[] args) {
+        new VowelDictionary().run();
         new FaultyUsers().run();
-    }
 }
