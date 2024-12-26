ff65786deaa696ffc81432355e98cafcee26b7b3
kungyu <hismayfly@gmail.com>
2024-12-27 00:14:53 +0900

    [rust] | [bevy] | reset learn-bevy

--- rust/bevy/learn-bevy/src/main.rs
+++ rust/bevy/learn-bevy/src/main.rs
@@ -1,12 +1,12 @@
 use bevy::prelude::*;
 
 pub mod plugins;
-pub mod robot;
+pub mod setup_kundoong;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
-        .add_systems(Startup, robot::setup)
-        .add_systems(Update, robot::animate_robot)
+        .add_systems(Startup, setup_kundoong::initial_setup)
+        .add_systems(Update, setup_kundoong::kundoong_keyboard_movement)
         .run();
 }
